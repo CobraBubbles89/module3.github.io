2320227a47111cc4e337c218a31d787e3736d2f2
Yurchik <vi060489jjv@gmail.com>
2023-07-24 23:03:55 +0300

    full site with adapt modal form

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
+$(document).ready(function () {
+  $('.carousel__inner').slick({
     speed: 1000,
     // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/chevron_left_solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="images/icons/chevron_right_solid.png"></button>',
-        responsive: [
-            {
-            breakpoint: 768,
-            settings: {
-                dots: false,
-                arrows: false
-      }
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/chevron_left_solid.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/chevron_right_solid.png"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          dots: false,
+          arrows: false
         }
+      }
     ]
-    });
+  });
     
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this)
-      .index()).addClass('catalog__content_active');
-    });
+        .index()).addClass('catalog__content_active');
+  });
   
   // $('.catalog-item__link').each(function (i) {
   //   $(this).on('click', function (e) {
@@ -40,16 +40,37 @@ $(document).ready(function(){
 
   function toggleSlide(item) {
     $(item).each(function (i) {
-    $(this).on('click', function (e) {
-      e.preventDefault();
-      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
     })
-  })
   };
 
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+  //modal
+  // $ чтобы получить элемент со страницы
+
+  $('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn();
+  });
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow')
+  });
+
+  $('.button_mini').on('click', function () {
+    $('.overlay, #order').fadeIn()
+  });
+
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn();
+    })
+  });
   
 });
 
